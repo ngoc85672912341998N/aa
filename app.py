c9bb9bae96420c27b40dd7fb25c3efbda4e65e33
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-28 17:15:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import httpx
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from models import GithubUserModel
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -13,19 +13,13 @@ timeout = httpx.Timeout(timeout=5.0, read=15.0)
 client = httpx.AsyncClient(limits=limits, timeout=timeout)
 
 
-
+@app.on_event("shutdown")
+async def shutdown_event():
+    print("shutting down...")
+    await client.aclose()
 
 
 @app.get("/", response_class=HTMLResponse)
-async def index(request: Request, username: str = None):
-    if not username:
-        return templates.TemplateResponse("index.html", context={"request": request})
+async def index(request: Request):
 
-    user = await get_github_profile(request, username)
-    if not user:
-        return templates.TemplateResponse("404.html", context={"request": request})
-
-    return templates.TemplateResponse("index.html", context={"request": request, "user": user})
-
-
-
+    return templates.TemplateResponse("index.html", context={"request": request})
