62522c294e145927587eabdb9f06f31317e96e2f
ngoc856729 <122215302+ngoc856729@users.noreply.github.com>
2023-07-28 18:16:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,70 @@
-from typing import Optional
-import httpx
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
-from models import bang_nhan_vien,update_data
-from sqlmodel import Session,select
-from sql_model import engine
-from sqlmodel import Field, Session, SQLModel, create_engine, select
-session=Session(bind=engine)
+from typing import List
+
+import databases
+import sqlalchemy
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# SQLAlchemy specific code, as with any other app
+DATABASE_URL = "sqlite:///./test.db"
+# DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+notes = sqlalchemy.Table(
+    "notes",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("text", sqlalchemy.String),
+    sqlalchemy.Column("completed", sqlalchemy.Boolean),
+)
+
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata.create_all(engine)
+
+
+class NoteIn(BaseModel):
+    text: str
+    completed: bool
+
+
+class Note(BaseModel):
+    id: int
+    text: str
+    completed: bool
+
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
-
-limits = httpx.Limits(max_keepalive_connections=30, max_connections=30)
-timeout = httpx.Timeout(timeout=30.0, read=30.0)
+limits = httpx.Limits(max_keepalive_connections=5, max_connections=10)
+timeout = httpx.Timeout(timeout=5.0, read=15.0)
 client = httpx.AsyncClient(limits=limits, timeout=timeout)
 
 
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
 @app.on_event("shutdown")
-async def shutdown_event():
-    print("shutting down...")
-    await client.aclose()
+async def shutdown():
+    await database.disconnect()
 
-@app.get("/theo_doi_nhan_su/", response_class=HTMLResponse)
-async def read_item(request: Request):
-    statement2 = select(update_data)
-    results2 = session.exec(statement2).all()
-    return results2
 
-@app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
+@app.get("/notes/", response_model=List[Note])
+async def read_notes():
+    query = notes.select()
+    return await database.fetch_all(query)
 
-    return templates.TemplateResponse("1.html", context={"request": request})
 
-@app.post("/thoi_gian_update/", response_model=update_data)
-def read_item(ngay:str,tinh_trang:str):
-    new_update = update_data(ngay_update=ngay, tinh_trang=tinh_trang)
-    session.add(new_update)
-    session.commit()
-    return new_update
+@app.post("/notes/", response_model=Note)
+async def create_note(note: NoteIn):
+    query = notes.insert().values(text=note.text, completed=note.completed)
+    last_record_id = await database.execute(query)
+    return {**note.dict(), "id": last_record_id}
